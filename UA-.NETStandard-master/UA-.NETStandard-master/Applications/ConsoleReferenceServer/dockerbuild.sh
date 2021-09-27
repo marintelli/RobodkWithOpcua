45b37a98b388124a090c39091dbbb87fca7405db
이용현 <lyh@uvc.co.kr>
2021-09-27 16:09:00 +0900

    back to scratch

--- UA-.NETStandard-master/UA-.NETStandard-master/Applications/ConsoleReferenceServer/dockerbuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo build a docker container of the .NET Core reference server
-dotnet build ConsoleReferenceServer.csproj
-dotnet publish ConsoleReferenceServer.csproj -o ./publish
-sudo docker build -t consolerefserver .
